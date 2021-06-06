f8a41b17fdb3e38481d1b906481e3a3fa4d66918
pawel.marcinowski11 <pawel.marcinowski11@gmail.com>
2021-06-06 14:04:15 +0200

    implement adding and updating student info

--- src/main/java/pl/polsl/controller/AddOrUpdateStudentsController.java
+++ src/main/java/pl/polsl/controller/AddOrUpdateStudentsController.java
@@ -3,6 +3,7 @@ package pl.polsl.controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import pl.polsl.Main;
 import pl.polsl.entities.Uczniowie;
 import pl.polsl.model.Student;
 
@@ -15,7 +16,7 @@ public class AddOrUpdateStudentsController implements ParametrizedController {
     public TextField poleDrugieImie;
     public TextField poleAdres;
     public TextField poleNazwisko;
-    public ComboBox poleKlasa;
+    //public ComboBox<Integer> poleKlasa;
 
 
     private Uczniowie modyfikowany;
@@ -50,22 +51,28 @@ public class AddOrUpdateStudentsController implements ParametrizedController {
         if (tryb == tr.Dodaj) {
             System.out.println("Dodawanie nowego ucznia");
             Uczniowie u = new Uczniowie();
-            u.setID(44);
-            u.setImie("Adam");
-            u.setNazwisko("Mickiewicz");
-            u.setAdres("Litwa");
+            //u.setID(44);
+            u.setImie(poleImie.getText());
+            u.setDrugieImie(poleDrugieImie.getText());
+            u.setNazwisko(poleNazwisko.getText());
+            u.setAdres(poleAdres.getText());
             u.setIdKlasy(1);
             (new Student()).persist(u);
         } else if (tryb == tr.Modyfikuj) {
             System.out.println("Modyfikowanie profilu ucznia");
+            modyfikowany.setImie(poleImie.getText());
+            modyfikowany.setDrugieImie(poleDrugieImie.getText());
+            modyfikowany.setNazwisko(poleNazwisko.getText());
+            modyfikowany.setAdres(poleAdres.getText());
+            //modyfikowany.setIdKlasy(poleKlasa.getSelectionModel().getSelectedItem());
             (new Student()).update(modyfikowany);
         }
-
+        Main.setRoot("manageStudentsForm");
     }
 
     public void discardChangesButton(ActionEvent event) throws IOException {
         System.out.println("Zmiany anulowano");
-
+        Main.setRoot("manageStudentsForm");
     }
 
 }
