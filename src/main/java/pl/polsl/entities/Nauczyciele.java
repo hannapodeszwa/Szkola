f239756249a97495969082888878cc691faf60f9
hannapodeszwa <hannapodeszwa@gmail.com>
2021-06-12 00:43:38 +0200

    generowanie id

--- src/main/java/pl/polsl/entities/Nauczyciele.java
+++ src/main/java/pl/polsl/entities/Nauczyciele.java
@@ -12,6 +12,7 @@ public class Nauczyciele implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue
     @Column(name = "ID", nullable = false)
     private Integer ID;
 
--- target/generated-sources/annotations/pl/polsl/entities/Konkursy_.java
+++ target/generated-sources/annotations/pl/polsl/entities/Konkursy_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-06-10T20:52:19")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-06-12T00:37:34")
 @StaticMetamodel(Konkursy.class)
 public class Konkursy_ { 
 
