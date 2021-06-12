6c0b3765a6dc64133370305be143324418caa2e9
Michał Opiełka <michalopielka17@gmail.com>
2021-06-12 09:29:22 +0200

    Added mail sender

---
+++ src/main/java/pl/polsl/model/EmailMessages.java
@@ -0,0 +1,10 @@
+package pl.polsl.model;
+
+public abstract class EmailMessages {
+
+    //Topics
+    static final String PASSWORD_RESET_CODE_TOPIC = "Password reset code.";
+
+    //Messages
+    static final String PASSWORD_RESET_CODE_MESSAGE = "Use this code to reset your password.";
+}
--- src/main/java/pl/polsl/model/Queries.java
+++ src/main/java/pl/polsl/model/Queries.java
@@ -2,5 +2,9 @@ package pl.polsl.model;
 
 public abstract class Queries {
 
+    //SignIn
     static final String GET_USER_BY_LOGIN_AND_PASSWORD = "SELECT C FROM Uzytkownicy C WHERE C.login = :LOGIN AND C.haslo = :PASSWORD";
+
+    //SignUp
+    static final String GET_USER_BY_LOGIN = "SELECT C FROM Uzytkownicy C WHERE C.login = :LOGIN";
 }
