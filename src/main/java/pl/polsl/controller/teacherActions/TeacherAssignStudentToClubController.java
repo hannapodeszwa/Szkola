0291c9ad17d00277e4abbb26410a0f71ec6fd8eb
Krystian Stebel <krystian1998k@gmail.com>
2021-06-19 14:33:43 +0200

    assigning students to clubs partially implemented

--- src/main/java/pl/polsl/controller/teacherActions/TeacherAssignStudentToClubController.java
+++ src/main/java/pl/polsl/controller/teacherActions/TeacherAssignStudentToClubController.java
@@ -2,26 +2,48 @@ package pl.polsl.controller.teacherActions;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import pl.polsl.Main;
 import pl.polsl.controller.ParametrizedController;
+import pl.polsl.entities.Klasy;
+import pl.polsl.entities.Uczniowie;
+import pl.polsl.model.SchoolClass;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TeacherAssignStudentToClubController implements ParametrizedController {
+    Integer clubId;
     Integer loggedTeacherId;
+
+    List<Klasy> classList;
 
     @FXML
     private ComboBox comboboxClass;
-    private TableView table;
+    @FXML
+    private TableView<Uczniowie> table;
+    @FXML
+    private TableColumn<Uczniowie, String> columnName;
+    @FXML
+    private TableColumn<Uczniowie, String> columnSurname;
+
+
 
     @Override
     public void receiveArguments(Map params) {
         loggedTeacherId = (Integer) params.get("id");
+        clubId = (Integer) params.get("clubId");
+
+        classList = (new SchoolClass()).displayClass();
+        if(!classList.isEmpty()) {
+            for (Klasy cl : classList) {
+                comboboxClass.getItems().add(cl.getNumer());
+            }
+            comboboxClass.getSelectionModel().select(0);
+
+        }
     }
 
     public void clickButtonBack(ActionEvent event) throws IOException
