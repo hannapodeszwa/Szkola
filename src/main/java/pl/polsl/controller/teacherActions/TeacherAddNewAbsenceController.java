5a833659febba0798d96fc6e974909e40abdaaab
Krystian Stebel <krystian1998k@gmail.com>
2021-06-21 01:17:06 +0200

    implement absence management function

--- src/main/java/pl/polsl/controller/teacherActions/TeacherAddNewAbsenceController.java
+++ src/main/java/pl/polsl/controller/teacherActions/TeacherAddNewAbsenceController.java
@@ -1,71 +1,116 @@
 package pl.polsl.controller.teacherActions;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import pl.polsl.Main;
 import pl.polsl.controller.ParametrizedController;
-import pl.polsl.entities.Kolanaukowe;
-import pl.polsl.model.ClubModel;
+import pl.polsl.entities.*;
+import pl.polsl.model.Grade;
+import pl.polsl.model.LessonTimeModel;
+import pl.polsl.model.Present;
+import pl.polsl.model.Teacher;
 import pl.polsl.utils.WindowSize;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class TeacherAddNewAbsenceController implements ParametrizedController {
-    Integer loggedTeacherId;
+
+
+
+    private Integer loggedTeacherId;
+    private Uczniowie student;
+
 
     @FXML
-    private TextField nameTextField;
+    private ComboBox<String> lessonComboBox;
+    @FXML
+    private ComboBox<String> excuseComboBox;
+    @FXML
+    private ComboBox<String> subjectComboBox;
+    @FXML
+    private Label labelStudent;
 
     @FXML
     private Label infoLabel;
 
+    ObservableList<GodzinyLekcji> timesList;
+    ObservableList<Przedmioty> subjectsList;
+
     @Override
-    public void receiveArguments(Map<String, Object> params) {
-        loggedTeacherId = (Integer) params.get("id");
-        infoLabel.setText("");
+    public void receiveArguments(Map<String, Object> params){
+        loggedTeacherId = (Integer) params.get("teacherId");
+
+
+        student = (Uczniowie) params.get("student");
+
+        subjectsList = (FXCollections.observableArrayList((new Teacher()).checkTeacherSubjects(loggedTeacherId)));
+
+
+
+
+        if(!subjectsList.isEmpty()){
+            for (Przedmioty p : subjectsList){
+                subjectComboBox.getItems().add(p.getNazwa());
+            }
+        }
+
+        labelStudent.setText(student.getImie() + " " + student.getNazwisko());
+
+        timesList = FXCollections.observableList((new LessonTimeModel()).getTime());
+
+        if(!timesList.isEmpty()){
+            for (GodzinyLekcji t : timesList){
+                String start = t.getPoczatek().toString();
+                String end = t.getKoniec().toString();
+                lessonComboBox.getItems().add(start + "-" + end);
+            }
+        }
+        lessonComboBox.getSelectionModel().select(0);
+        excuseComboBox.getSelectionModel().select(0);
+        subjectComboBox.getSelectionModel().select(0);
+
     }
 
-    public void clickButtonBack() throws IOException
+
+
+
+
+
+    public void backAction() throws IOException
     {
         Map<String, Object> params = new HashMap<>();
         params.put("id", loggedTeacherId);
-        Main.setRoot("teacherActions/teacherClubForm", params, WindowSize.teacherClubForm);
+        Main.setRoot("teacherActions/teacherAbsenceForm", params, WindowSize.teacherGradeForm);
+    }
+    public void submitAction()
+    {
+
+            addNewAbsence();
+            infoLabel.setText("Sukces!"+ System.lineSeparator() + "Lekcja: " + lessonComboBox.getValue().toString() + System.lineSeparator() + "Usprawiedliwiona: " + excuseComboBox.getValue().toString() + System.lineSeparator() );
+
+
     }
 
-    public void clickButtonAdd() throws IOException {
-        infoLabel.setText("");
-        String name = nameTextField.getText();
-        if(name.isEmpty()){
-            infoLabel.setText("Nazwa koła naukowego nie może być pusta!");
-        } else {
-            ObservableList<Kolanaukowe> clubList = FXCollections.observableList((new ClubModel()).findAll());
-            if(clubList.stream().anyMatch(c -> c.getOpis().equals(name))){
-                infoLabel.setText("Błąd! Koło naukowe " + name + " już istnieje!");
-            } else {
-                Kolanaukowe kl = new Kolanaukowe();
-                kl.setOpis(name);
-                kl.setIdNauczyciela(loggedTeacherId);
-                (new ClubModel()).persist(kl);
-                infoLabel.setText("Koło naukowe " + name + " utworzono pomyślnie");
-            }
-        }
+    private void addNewAbsence() {
+        Nieobecnosci a = new Nieobecnosci();
+
+        a.setData(new Date(System.currentTimeMillis()));
+        a.setPrzedmiotyId(subjectsList.get(subjectComboBox.getSelectionModel().getSelectedIndex()).getID());
+        a.setUczniowieId(student.getID());
+
+        Integer tid = timesList.get(lessonComboBox.getSelectionModel().getSelectedIndex()).getID();
+        a.setGodzina(tid);
+
+        a.setCzyUsprawiedliwiona(excuseComboBox.getSelectionModel().getSelectedIndex());
+        (new Present()).persist(a);
     }
 
-    private boolean checkIfExists(String name){
-        Boolean ifExistsAlready = false;
-        List<Kolanaukowe> clubList = (new ClubModel()).findAll();
-        for(Kolanaukowe k : clubList){
-            if(k.getOpis() == name){
-                ifExistsAlready = true;
-            }
-        }
-        return ifExistsAlready;
-    }
 
 }
