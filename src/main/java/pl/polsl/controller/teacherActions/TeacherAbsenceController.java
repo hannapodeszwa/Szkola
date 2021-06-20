92f7c1a564756d4e0e30241303915b269398a0e4
Krystian Stebel <krystian1998k@gmail.com>
2021-06-20 09:18:41 +0200

    Create forms and controllers for competitions and absences

---
+++ src/main/java/pl/polsl/controller/teacherActions/TeacherAbsenceController.java
@@ -0,0 +1,196 @@
+package pl.polsl.controller.teacherActions;
+
+
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
+import pl.polsl.Main;
+import pl.polsl.controller.ParametrizedController;
+import pl.polsl.entities.Klasy;
+import pl.polsl.entities.Oceny;
+import pl.polsl.entities.Przedmioty;
+import pl.polsl.entities.Uczniowie;
+import pl.polsl.model.Grade;
+import pl.polsl.model.Student;
+import pl.polsl.model.Teacher;
+import pl.polsl.utils.WindowSize;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TeacherAbsenceController implements ParametrizedController {
+
+    @FXML
+    public TableView<Oceny> table;
+//    public TableColumn<Uwagi, Text> columnDesc;
+    public TableColumn<Oceny, String> columnDesc;
+    public TableColumn<Oceny, String> columnDate;
+    public TableColumn<Oceny, String> columnGrade;
+    public TableColumn<Oceny, String> columnValue;
+    public ComboBox<String> comboboxClass;
+    public ComboBox<String> comboboxStudent;
+    public ComboBox<String> comboboxSubject;
+    public Button buttonDelete;
+    public Button buttonAdd;
+    public Button buttonBack;
+
+    Integer id;
+    List<Klasy> classList;
+    ObservableList<Uczniowie> studentList;
+    ObservableList<Przedmioty> subjectsList;
+    ObservableList<Oceny> gradeList;
+
+    @Override
+    public void receiveArguments(Map params) {
+        id = (Integer) params.get("id");
+        classList = (new Teacher()).checkTeacherClasses(id);
+        subjectsList = (FXCollections.observableArrayList((new Teacher()).checkTeacherSubjects(id)));
+
+        if(!subjectsList.isEmpty()) {
+            for (Przedmioty sl : subjectsList) {
+                comboboxSubject.getItems().add(sl.getNazwa());
+            }
+            comboboxSubject.getSelectionModel().select(0);
+        }
+
+        if(!classList.isEmpty()) {
+            for (Klasy cl : classList) {
+                comboboxClass.getItems().add(cl.getNumer());
+            }
+            comboboxClass.getSelectionModel().select(0);
+            setStudents(0);
+            setGrade();
+        }
+
+    }
+
+    public void clickButtonDelete() {
+        Oceny o = table.getSelectionModel().getSelectedItem();
+        (new Grade()).delete(o);
+    }
+
+    public void clickButtonAdd() throws IOException{
+        Map params = new HashMap<String, Object>();
+
+        params.put("teacherId", id);
+
+        Integer index = comboboxStudent.getSelectionModel().getSelectedIndex();
+        params.put("studentId", studentList.get(index).getID());
+        params.put("subjectId", subjectsList.get(index).getID());
+        params.put("surname", studentList.get(index).getNazwisko());
+        params.put("name", studentList.get(index).getImie());
+        params.put("subject",subjectsList.get(index).getNazwa());
+
+        Main.setRoot("teacherActions/teacherAddNewGradeForm", params, WindowSize.teacherAddNewGradeForm);
+    }
+
+    void setStudents(Integer index){
+        studentList = FXCollections.observableArrayList((new Student()).getStudentInClass(classList.get(index).getID()));
+        if(!studentList.isEmpty()){
+            for (Uczniowie student : studentList) {
+                comboboxStudent.getItems().add(student.getImie()+" " + student.getNazwisko());
+            }
+            comboboxStudent.getSelectionModel().select(0);
+        }
+    }
+
+    public void changeComboboxClass() {
+        studentList.clear();
+        comboboxStudent.getItems().clear();
+        setStudents(comboboxClass.getSelectionModel().getSelectedIndex());
+    }
+
+    public void changeComboboxSubject() {
+        gradeList.clear();
+        setGrade();
+    }
+
+    public Integer getWitdh(String text){
+        return (int)(new Text(text)).getBoundsInLocal().getWidth();
+    }
+
+    public String wrapString(String wraping,Integer wid){
+        Integer width = wid-10;
+        if(getWitdh(wraping) < width) {
+            return wraping;
+        }
+        String result = "";
+        String[] words = wraping.split(" ");
+        List<Integer> sizewords = new ArrayList<Integer>();
+
+        for(String word : words){
+            sizewords.add(getWitdh(word));
+        }
+        Integer act = 0;
+        Integer i = 0;
+        for(Integer size : sizewords) {
+
+            if(size + act < width){
+                result += words[i] + " ";
+                act +=size+3;
+            }
+            else if (size + act >= width) {
+                result += "\n" + words[i]+" ";
+                act=size+3;
+            }
+            else {
+                result += "\n" + words[i] + "\n";
+                act = 0;
+            }
+            i++;
+        }
+
+
+        return result;
+    }
+
+    void setGrade(){
+        gradeList = FXCollections.observableArrayList((new Grade()).checkStudentGrades(studentList.get(comboboxStudent.getSelectionModel().getSelectedIndex()), subjectsList.get(comboboxSubject.getSelectionModel().getSelectedIndex())));
+
+
+        columnDesc.setCellValueFactory(CellData -> {
+            String tym = CellData.getValue().getOpis();
+            return new ReadOnlyStringWrapper(wrapString(tym, (int)columnDesc.getWidth()));
+        });
+
+        columnDate.setCellValueFactory(CellData -> {
+            String tym = CellData.getValue().getData().toString();
+            return new ReadOnlyStringWrapper(wrapString(tym, (int)columnDate.getWidth()));
+        });
+
+        columnValue.setCellValueFactory(CellData -> {
+            String tym = CellData.getValue().getWaga().toString().toString();
+            return new ReadOnlyStringWrapper(wrapString(tym, (int)columnValue.getWidth()));
+        });
+
+        columnGrade.setCellValueFactory(CellData -> {
+            String tym = CellData.getValue().getOcena().toString();
+            return new ReadOnlyStringWrapper(wrapString(tym, (int)columnGrade.getWidth()));
+        });
+
+        table.setItems(gradeList);
+    }
+
+    public void changeComboboxStudent() {
+        gradeList.clear();
+        setGrade();
+
+    }
+
+    public void clickButtonBack() throws IOException {
+        Map params = new HashMap<String, String>();
+        params.put("id", id);
+        Main.setRoot("menu/teacherMenuForm", params);
+    }
+
+
+}
---
+++ src/main/java/pl/polsl/controller/teacherActions/TeacherCompetitionController.java
@@ -0,0 +1,142 @@
+package pl.polsl.controller.teacherActions;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import pl.polsl.Main;
+import pl.polsl.controller.ParametrizedController;
+import pl.polsl.entities.Kolanaukowe;
+import pl.polsl.entities.Nauczyciele;
+import pl.polsl.entities.Uczniowie;
+import pl.polsl.entities.Udzialwkole;
+import pl.polsl.model.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TeacherCompetitionController implements ParametrizedController {
+    Integer loggedTeacherId;
+
+    ObservableList<Kolanaukowe> clubsList;
+    ObservableList<Uczniowie> studentList;
+
+    @FXML
+    private ComboBox comboboxClubs;
+    @FXML
+    private TableColumn<Uczniowie, String> columnStudent;
+    @FXML
+    private TableColumn<Uczniowie, String> columnClass;
+    @FXML
+    private TableView<Uczniowie> table;
+    @FXML
+    private Label infoLabel;
+
+    private void refreshClubList(){
+        if(clubsList.isEmpty()) {
+            ObservableList<String> emp = null;
+            emp.add("Brak kół zainteresowań");
+            comboboxClubs.setItems(emp);
+        } else {
+            for(Kolanaukowe knl : clubsList) {
+                comboboxClubs.getItems().add(knl.getOpis());
+            }
+            comboboxClubs.getSelectionModel().select(0);
+            setParticipants(0);
+        }
+    }
+    @Override
+    public void receiveArguments(Map params) {
+        loggedTeacherId = (Integer) params.get("id");
+
+        Nauczyciele n = (new Teacher()).getTeacherById(loggedTeacherId);
+        clubsList = FXCollections.observableArrayList((new ClubModel()).findByTeacher(n));
+
+        refreshClubList();
+
+        infoLabel.setText("");
+    }
+
+    public void clickButtonBack(ActionEvent event) throws IOException
+    {
+        Map params = new HashMap<String, String>();
+        params.put("id", loggedTeacherId);
+        Main.setRoot("menu/teacherMenuForm", params);
+    }
+
+    public void clickButtonAdd(ActionEvent event) throws IOException {
+        Map params = new HashMap<String, String>();
+        params.put("id", loggedTeacherId);
+        Main.setRoot("teacherActions/teacherAddNewClubForm", params);
+    }
+
+    public void clickButtonAssign(ActionEvent event) throws IOException {
+        Map params = new HashMap<String, String>();
+        Integer tmpId = comboboxClubs.getSelectionModel().getSelectedIndex();
+        Kolanaukowe k =  clubsList.get(tmpId);
+        Integer clubId = k.getID();
+        params.put("id", loggedTeacherId);
+        params.put("clubId", clubId);
+        Main.setRoot("teacherActions/teacherAssignStudentToClubForm", params);
+    }
+
+    public void clickButtonDelete() {
+        if(studentList.isEmpty()){
+            Integer tmpId = comboboxClubs.getSelectionModel().getSelectedIndex();
+            Kolanaukowe k =  clubsList.get(tmpId);
+            (new ClubModel()).delete(k);
+            refreshClubList();
+        } else {
+            infoLabel.setText("Usuń wszystkich\nuczestników przed\nusunięciem koła naukowego!");
+        }
+    }
+
+    public void clickButtonUnassign() {
+     /*   Oceny o = table.getSelectionModel().getSelectedItem();
+        (new Grade()).delete(o); */
+        Integer tmpId = comboboxClubs.getSelectionModel().getSelectedIndex();
+        Kolanaukowe k =  clubsList.get(tmpId);
+        List<Udzialwkole> participantsList = (new ClubParticipationModel()).findByClub(k);
+
+        Integer uid = table.getSelectionModel().getSelectedItem().getID();
+
+
+        Udzialwkole tmp = participantsList.stream().filter(p -> p.getIdUcznia().equals(uid)).findAny().orElse(null);
+        if(tmp != null) {
+            
+            (new ClubParticipationModel()).delete(tmp);
+        }
+        studentList.clear();
+        refreshClubList();
+        setParticipants(tmpId);
+    }
+
+    public void changeComboboxClubs(ActionEvent event) throws IOException {
+        studentList.clear();
+        setParticipants(comboboxClubs.getSelectionModel().getSelectedIndex());
+    }
+
+    private void setParticipants(int index) {
+
+        Integer clubId = clubsList.get(index).getID();
+
+        studentList = FXCollections.observableArrayList((new Student()).getStudentInClub(clubId));
+
+   //     System.out.println(clubId + " " + studentList.get(0).getImie());
+
+        columnClass.setCellValueFactory(CellData -> new SimpleStringProperty((new SchoolClass()).getClassById(CellData.getValue().getIdKlasy()).getNumer()));
+        columnStudent.setCellValueFactory(CellData -> new SimpleStringProperty(CellData.getValue().getImie() + " " + CellData.getValue().getNazwisko()));
+
+        table.setItems(studentList);
+    }
+
+
+
+}
