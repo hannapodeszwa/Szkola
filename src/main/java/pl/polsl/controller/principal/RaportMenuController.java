d6ef8d4abb926c7e1c4144899489d06ad9259dfd
pawel.marcinowski11 <pawel.marcinowski11@gmail.com>
2021-06-27 23:53:47 +0200

    implement printing

--- src/main/java/pl/polsl/controller/principal/RaportMenuController.java
+++ src/main/java/pl/polsl/controller/principal/RaportMenuController.java
@@ -18,6 +18,9 @@ public class RaportMenuController {
         List l=s.getGradeFromSubject(p);
     }
 
+    public void clickButtonSelectStudent(ActionEvent actionEvent) {
+        (new HelloWorldPrinter()).printingCall();
+    }
 
     public void clickButtonBack(ActionEvent event) throws IOException {
         Main.setRoot("menu/adminMenuForm.fxml",
@@ -62,5 +65,3 @@ class HelloWorldPrinter implements Printable {
         }
     }
 }
-
-
